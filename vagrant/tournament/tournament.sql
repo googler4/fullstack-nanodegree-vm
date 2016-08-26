99ff8d1ea05678f362c3200d36361d6bcb7fef3f
googler4 <username@users.noreply.github.com>
2016-08-26 09:56:37 -0700

    updated psql keys (primary and foriegn) & Init DB

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -5,25 +5,32 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
+DROP DATABASE IF EXISTS tournament;
+CREATE DATABASE tournament;
+
+\c tournament
+
 DROP TABLE IF EXISTS players;
 CREATE TABLE players ( name TEXT,
                      tourn_ids integer[],
                      time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                     id SERIAL );
+                     id SERIAL primary key);
+
+--It's not annoying it's 
 
 DROP TABLE IF EXISTS matches;
 CREATE TABLE matches ( title TEXT,
                      player_ids integer[],
                      tourn_id integer,
-                     winner integer,
-                     loser integer,
+                     winner integer references players(id),
+                     loser integer references players(id),
                      ties integer [],
                      time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                     id SERIAL );
+                     id SERIAL primary key);
 
 DROP TABLE IF EXISTS tournaments;
 CREATE TABLE tournaments (
-                     tourn_name TEXT,
+                     tourn_name TEXT ,
                      tourn_live BOOLEAN,
                      time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                     id SERIAL );
+                     id SERIAL primary key);
